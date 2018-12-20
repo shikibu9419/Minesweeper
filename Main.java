dee87b6d8e8bae7bd55678bd5f14454bbb978fc4
Kazuya Izumi <shikibu9419@gmail.com>
2018-12-20 15:13:41 +0900

    Metcha henkou shimashita m(_ _)m

---
+++ Main.java
@@ -0,0 +1,9 @@
+import ui.*;
+// import control.*;
+// import informations.*;
+
+public class Main {
+  public static void main(String[] args) {
+    new Display().start();
+  }
+}
--- ui/Display.java
+++ ui/Display.java
@@ -1,32 +1,46 @@
+package ui;
+
 import java.util.*;
 import java.io.*;
 
-public class Display{
-  public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
+public class Display {
+    private Scanner scan;
 
-    String input;
+    public Display() {
+        scan = new Scanner(System.in);
+    }
 
-    for(int i = 0; i < 20; i++){
-      for(int j = 0; j < 20; j++){
-        System.out.print("_|");
-      }
-      System.out.println("");
+    public void start() {
+        while(true) {
+            display();
+            String input = scan.next();
+            System.out.println(input);
+        }
     }
-    
-    System.out.print("> ");
-    input = scan.next();
-    try{
-      clearScreen();
-      System.out.println(input);
-    }catch(IOException e){
-      System.out.println("IO Error");
-    }catch(InterruptedException e){
-      System.out.println("I.R. Error");
+
+    private void display() {
+        clearScreen();
+
+        for(int i = 0; i < 20; i++) {
+            for(int j = 0; j < 20; j++)
+                System.out.print("|   ");
+            System.out.print("|");
+            System.out.println("");
+            for(int j = 0; j < 20; j++)
+                System.out.print("----");
+            System.out.print("-");
+            System.out.println("");
+        }
+        System.out.print("> ");
     }
-  }
-  public static void clearScreen() throws IOException, InterruptedException{
-    new ProcessBuilder("/bin/bash","-c","clear").inheritIO().start().waitFor();
-  }
+
+    public void clearScreen() {
+        try {
+            new ProcessBuilder("/bin/bash","-c","clear").inheritIO().start().waitFor();
+        } catch(IOException e) {
+            System.out.println("IO Error");
+        } catch(InterruptedException e) {
+            System.out.println("I.R. Error");
+        }
+    }
 }
-
