ad5b0ade1519fd81810d1b2ef86cb558323c53c4
Kazuya Izumi <shikibu9419@gmail.com>
2019-01-10 12:09:01 +0900

    Fix bug

--- control/Field.java
+++ control/Field.java
@@ -8,13 +8,13 @@ public class Field extends Control {
     // ゲームにつき1つのためクラス変数で管理する子たち
     public static final int MAX_Y = 30;
     public static final int MAX_X = 30;
-    public static final int MINE_COUNT = 600;
+    public static final int MINE_COUNT = 500;
     public static Cell[][] fieldmap = new Cell[MAX_Y][MAX_X];
     public static Unit unit;
 
     // 本体に影響がないようにfieldmapのディープコピーを渡すメソッド
     public static Cell[][] getClone() {
-        res = new Cell[MAX_Y][MAX_X];
+        Cell[][] res = new Cell[MAX_Y][MAX_X];
         for(int i = 0; i < MAX_Y; i++)
             for(int j = 0; j < MAX_X; j++)
                 res[i][j] = fieldmap[i][j].clone();
@@ -42,8 +42,8 @@ public class Field extends Control {
 
             // スタート周辺orゴールor平地でない ときは飛ばす
             if((y < 2 && x < 2) ||
-                    (y == MAX_Y - 1 && x == MAX_X - 1) ||
-                    !(fieldmap[y][x] instanceof Flatland))
+               (y == MAX_Y - 1 && x == MAX_X - 1) ||
+               !(fieldmap[y][x] instanceof Flatland))
                 continue;
 
             Mine mine = new Mine(y, x);  // 地雷を設置
