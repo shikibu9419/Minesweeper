49dfb42df88bece27a5ad4d6fc1fe73fa8766d13
Kazuya Izumi <shikibu9419@gmail.com>
2019-01-10 10:52:38 +0900

    Move clone method to control.Field

--- control/Field.java
+++ control/Field.java
@@ -6,21 +6,35 @@ import models.*;
 public class Field extends Control {
 
     // ゲームにつき1つのためクラス変数で管理する子たち
-    public static final int MAX_Y = 20;
-    public static final int MAX_X = 20;
-    public static final int MINE_COUNT = 75;
+    public static final int MAX_Y = 30;
+    public static final int MAX_X = 30;
+    public static final int MINE_COUNT = 600;
     public static Cell[][] fieldmap = new Cell[MAX_Y][MAX_X];
     public static Unit unit;
+
+    // 本体に影響がないようにfieldmapのディープコピーを渡すメソッド
+    public static Cell[][] getClone() {
+        res = new Cell[MAX_Y][MAX_X];
+        for(int i = 0; i < MAX_Y; i++)
+            for(int j = 0; j < MAX_X; j++)
+                res[i][j] = fieldmap[i][j].clone();
+        return res;
+    }
 
     // fieldの初期化
     public static void initFieldmap() {
         for(int i = 0; i < MAX_Y; i++)
             for(int j = 0; j < MAX_X; j++)
                 fieldmap[i][j] = new Flatland();
+        setUnits();
+        setMines();
+    }
 
-        unit = new Unit(0, 0);  // 操作可能ユニットを配置
+    private static void setUnits() {
+        unit = new Unit(0, 0);
+    }
 
-        // ランダムな場所に地雷を配置
+    private static void setMines() {
         int count = 0;
         while(count < MINE_COUNT) {
             int y = randomInt(MAX_Y);
@@ -28,8 +42,8 @@ public class Field extends Control {
 
             // スタート周辺orゴールor平地でない ときは飛ばす
             if((y < 2 && x < 2) ||
-               (y == MAX_Y - 1 && x == MAX_X - 1) ||
-               !(fieldmap[y][x] instanceof Flatland))
+                    (y == MAX_Y - 1 && x == MAX_X - 1) ||
+                    !(fieldmap[y][x] instanceof Flatland))
                 continue;
 
             Mine mine = new Mine(y, x);  // 地雷を設置
--- ui/animations/Animation.java
+++ ui/animations/Animation.java
@@ -9,19 +9,8 @@ public class Animation extends UI {
 
     protected Cell[][] fieldmap;
 
-    // 本体に影響がないようにfieldmapのディープコピーを渡す
     public Animation() {
-        setFieldmap();
-    }
-
-    private void setFieldmap() {
-        final int MAX_Y = Field.fieldmap.length;
-        final int MAX_X = Field.fieldmap[0].length;
-
-        fieldmap = new Cell[MAX_Y][MAX_X];
-        for(int i = 0; i < MAX_Y; i++)
-            for(int j = 0; j < MAX_X; j++)
-                fieldmap[i][j] = Field.fieldmap[i][j].clone();
+        fieldmap = Field.getClone();
     }
 
     // sec * 0.1 秒待機
