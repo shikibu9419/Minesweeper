1ed8637880f54263fe67dd8dcf0647f3a8c36e18
yuyaieda <yuya@macbookpro.local>
2019-01-23 16:14:03 +0900

    bombed追加

--- algorithm/Opponent.java
+++ algorithm/Opponent.java
@@ -11,23 +11,78 @@ public class Opponent extends Algorithm {
     }
 
     public void start() {
-        Information.addNotification("OPPONENT'S TURN:");
-
         for(Unit enemy:Information.enemies) {
             if(enemy.dead)
                 continue;
-            if(moveToClosestAlly(enemy))
+
+            if(bombToAlly(enemy)){
                 System.out.println("");
+            }else if(moveToClosestAlly(enemy)){
+                System.out.println("");
+            }
+        }
+    }
+
+    private boolean bombToAlly(Unit enemy){
+
+        UnitAction action = new UnitAction(enemy);
+        for (Unit ally:Information.allies){
+            if(ally.dead)
+                continue;
+
+            int maxMines = 0;
+            int mine_x = 10000;
+            int mine_y = 10000;
+
+            for(int i = -1; i < 2; i++){
+                for (int j = -1; j < 2; j++){
+                    if (i != 0 || j != 0){
+                        if ((ally.x + i) < Information.MAX_X && (ally.y + j) < Information.MAX_Y && (ally.x + i) >= 0 && (ally.y + j) >= 0){
+                            if (getMaxNumberOfMines(ally.y + j, ally.x + i) > maxMines){
+                                if ((enemy.x - Information.AVAILABLE_RANGE) <= (ally.x + i) && (ally.x + i) < (enemy.x + Information.AVAILABLE_RANGE) && (enemy.y - Information.AVAILABLE_RANGE) <= (ally.y + j) && (ally.y + j) < (enemy.y + Information.AVAILABLE_RANGE)){
+                                    maxMines = getMaxNumberOfMines(ally.y + j, ally.x + i);
+                                    mine_x = ally.x + i;
+                                    mine_y = ally.y + j;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.print ((mine_x + 1) + " " + (mine_y + 1) + "  ");
+            System.out.println (maxMines);
+            if ((mine_x != 10000) && Information.fieldmap[mine_y][mine_x].bombed == false){
+                Information.fieldmap[mine_y][mine_x].bombed = true;
+                return action.detonate(mine_y, mine_x);
+            }
+        }
+        return false;
+    }
+
+    //x, y周囲8マスの数字マスの最大値を取得
+    private int getMaxNumberOfMines(int y, int x){
+        //数字マスには地雷はない
+        if (Information.fieldmap[y][x].surroundMines > 0){
+            return 0;
         }
 
-        Information.addNotification("");
+        int numberOfMines = 0;
+        for(int i = -1; i < 2; i++){
+            for (int j = -1; j < 2; j++){
+                if ((x + i) < Information.MAX_X && (y + j) < Information.MAX_Y && (x + i) >= 0 && (y + j) >= 0){
+                    numberOfMines = Math.max(Information.fieldmap[y + j][x + i].surroundMines, numberOfMines);
+                }
+            } 
+        }
+        return numberOfMines;
     }
 
     // 敵ユニットの動き(暫定実装)
     private boolean moveToClosestAlly(Unit enemy) {
-        Unit closest;
+
         int disy = Information.MAX_Y;
         int disx = Information.MAX_X;
+        Unit closest;
         UnitAction action = new UnitAction(enemy);
 
         // 一番近いユニットとその距離を計算
